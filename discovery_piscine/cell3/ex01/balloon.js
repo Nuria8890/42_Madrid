0074e47dc7b42b10016dc87704b2e43393e57cf8
Nuria Campo <nuria.campo7@gmail.com>
2024-02-26 20:03:52 +0100

    ballon crece y cambia de color

--- discovery_piscine/cell3/ex01/balloon.js
+++ discovery_piscine/cell3/ex01/balloon.js
@@ -1,9 +1,27 @@
+function pincha() {
+    crece10();
+    cambiaColor();
+}
+
+
 function crece10() {
     const balloonObj = document.getElementById("balloon")
     var tamano = balloonObj.clientWidth + 10 + "px";
     balloonObj.style.width = tamano;
     balloonObj.style.height = tamano;
-    
-    
     console.log(balloonObj)
+}
+
+const color = ["red", "green", "blue"];
+var colorActual = 1;
+
+function cambiaColor() {
+    const balloonObj = document.getElementById("balloon")
+    balloonObj.style.background = color[colorActual];
+    
+    colorActual = colorActual + 1;
+    if (colorActual === color.length){
+        colorActual = 0;
+    }
+    console.log(colorActual);
 }
