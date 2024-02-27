97c937ff1115bcc1ff2459d861c7adba94ce9811
Nuria Campo <nuria.campo7@gmail.com>
2024-02-27 13:50:20 +0100

    baloon crece y cambia de color bien hecho

--- discovery_piscine/cell3/ex01/balloon.js
+++ discovery_piscine/cell3/ex01/balloon.js
@@ -5,23 +5,28 @@ function pincha() {
 
 
 function crece10() {
-    const balloonObj = document.getElementById("balloon")
+    const balloonObj = document.getElementById("balloon");
     var tamano = balloonObj.clientWidth + 10 + "px";
     balloonObj.style.width = tamano;
     balloonObj.style.height = tamano;
+
     console.log(balloonObj)
 }
 
+
 const color = ["red", "green", "blue"];
-var colorActual = 1;
+var colorActual = 0;
 
 function cambiaColor() {
-    const balloonObj = document.getElementById("balloon")
-    balloonObj.style.background = color[colorActual];
-    
-    colorActual = colorActual + 1;
-    if (colorActual === color.length){
-        colorActual = 0;
+    const balloonObj = document.getElementById("balloon");
+
+    colorSiguiente = colorActual + 1;
+    if (colorActual === color.length - 1){
+        colorSiguiente = 0;
     }
-    console.log(colorActual);
+    console.log("antes de cambiar " + colorActual);
+
+    balloonObj.style.background = color[colorSiguiente];
+    colorActual = colorSiguiente;
+    console.log("después de cambiar: " +colorActual);
 }
