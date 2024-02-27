76881acfefa86c88b7e270ab5d2b5a223a0ba6c7
Nuria Campo <nuria.campo7@gmail.com>
2024-02-27 14:45:32 +0100

    balloon explota con efecto

--- discovery_piscine/cell3/ex01/balloon.js
+++ discovery_piscine/cell3/ex01/balloon.js
@@ -7,6 +7,7 @@ function pincha() {
 
 function crece10() {
     const balloonObj = document.getElementById("balloon");
+    balloonObj.className = ""; // al finalizar function explota() continúa enlazando la clase efecto, por eso en este punto pongo clase vacía, para que no se repita el efecto de explosión.
     var tamano = balloonObj.clientWidth + 10 + "px";
     balloonObj.style.width = tamano;
     balloonObj.style.height = tamano;
@@ -40,7 +41,12 @@ function explota() {
     console.log(tamanoBoom);
 
     if (tamanoBoom >= 420) {
+        balloonObj.className = "efecto";
         balloonObj.style.width = 200 + "px";
         balloonObj.style.height = 200 + "px";
     }
 }
+
+
+
+
