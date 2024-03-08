dc4c97bb282d8348812383c20d82dda70cbe085d
Nuria Campo <nuria.campo7@gmail.com>
2024-03-08 13:49:59 +0100

    al hace click en New se crean nuevos elementos al inicio de la lista

--- discovery_piscine/cell3/ex03/todo.js
+++ discovery_piscine/cell3/ex03/todo.js
@@ -1,4 +1,23 @@
 
+//CREAR UN NUEVO ELEMENTO EN LA LISTA
+const newLi = document.getElementById("btn");
+
+btn.onclick = function(){
+    console.log("estás haciendo click en New")
+    
+    let textoNewTarea = prompt("Indica el nombre de tu nueva tarea");
+    let ol = document.getElementById("ft_list");
+    
+    if(textoNewTarea){
+        var newTarea = document.createElement("li");
+        ol.insertBefore(newTarea, ol.firstElementChild);
+        newTarea.appendChild(document.createTextNode(textoNewTarea))
+        console.log("has creado una nueva tarea");
+    }
+    }
+
+
+//ELIMINAR ELEMENTO DE LA LISTA
 const liArray = document.querySelectorAll("li");
 
 for (var i = 0; i < liArray.length; i++) {
@@ -16,8 +35,8 @@ liArray.forEach(function(li) {
     li.onclick = function() {
         console.log("estás haciendo click en la tarea " + this.outerText);
         if(confirm("¿Estás segur@ de que deseas eliminar esta tarea?")){
-            this.remove()
-            console.log("has eliminado la tarea " + this.outerText)
+            this.remove();
+            console.log("has eliminado la tarea " + this.outerText);
         }
     }
 }
