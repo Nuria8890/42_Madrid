cff8411d1207013ea454c8e1952f5349e801e470
Nuria Campo <nuria.campo7@gmail.com>
2024-03-21 11:59:39 +0100

    balloon con jQuery

--- discovery_piscine/cell3/ex04/ex01bis/balloonBIS.js
+++ discovery_piscine/cell3/ex04/ex01bis/balloonBIS.js
@@ -1,33 +1,34 @@
-function pincha() {
+$('#balloon').on('click', function () {
     cambiaTamano("sumar");
     cambiaColor("ordenado");
     explota();
-}
+})
 
-function ratonSale() {
+$('#balloon').on('mouseout', function () {
     cambiaTamano("restar");
     cambiaColor("inverso");
-}
+})
 
-function cambiaTamano(accion) {
-    const balloonObj = document.getElementById("balloon");
-    balloonObj.className = ""; // al finalizar function explota() continúa enlazando la clase efecto, por eso en este punto pongo clase vacía, para que no se repita el efecto de explosión.
+function cambiaTamano(accion){
+    const balloonObj = $("#balloon");
+    
+    balloonObj.removeClass("efecto"); // al finalizar function explota() continúa enlazando la clase efecto, por eso en este punto elimino la clase efecto, para que no se repita el efecto de explosión.
 
     if (accion === "sumar") {
-        var tamano = balloonObj.clientWidth + 10;
-        console.log("Has pinchado en el balloon y ahora mido " + tamano)
+        var tamano = balloonObj.width() + 10;
+        console.log("Has pinchado en el balloon y ahora mido " + tamano + "px");
     }
     
 
     if (accion === "restar") {
-        var tamano = balloonObj.clientWidth -5;
-        console.log("Has salido del balloon y ahora mido " + tamano)
+        var tamano = balloonObj.width() -5;
+        console.log("Has salido del balloon y ahora mido " + tamano + "px");
     }
     
 
     if (tamano >= 200) {
-        balloonObj.style.width = tamano + "px";
-        balloonObj.style.height = tamano + "px";
+        balloonObj.width(tamano);
+        balloonObj.height(tamano);
     }
 }
 
@@ -35,14 +36,14 @@ const color = ["red", "green", "blue"];
 var colorAnterior = 0;
 
 function cambiaColor(sentido) {
-    const balloonObj = document.getElementById("balloon");
+    const balloonObj = $("#balloon");
 
     if (sentido === "ordenado") {
         colorSiguiente = colorAnterior + 1;
         if (colorAnterior === color.length - 1){
             colorSiguiente = 0;
         }
-        console.log("Has pinchado en el balloon y ahora soy de color " + color[colorSiguiente])
+        console.log("Has pinchado en el balloon y ahora soy de color " + color[colorSiguiente]);
     }
 
     if (sentido === "inverso") {
@@ -50,23 +51,23 @@ function cambiaColor(sentido) {
         if (colorAnterior === 0){
             colorSiguiente = color.length - 1;
         }
-        console.log("Has salido del balloon y ahora soy de color " + color[colorSiguiente])
+        console.log("Has salido del balloon y ahora soy de color " + color[colorSiguiente]);
     }
 
-    balloonObj.style.background = color[colorSiguiente];
+    balloonObj.css("background", color[colorSiguiente]);
     colorAnterior = colorSiguiente;
 }
 
 
 
 function explota() {
-    const balloonObj = document.getElementById("balloon");
-    var tamanoBoom = balloonObj.clientWidth;
+    const balloonObj = $("#balloon");
+    var tamanoBoom = balloonObj.width();
 
     if (tamanoBoom > 420) {
-        balloonObj.className = "efecto";
-        balloonObj.style.width = 200 + "px";
-        balloonObj.style.height = 200 + "px";
+        balloonObj.addClass("efecto");
+        balloonObj.width(200);
+        balloonObj.height(200);
     }
-    // al finalizar function explota() continúa enlazando la clase efecto, por eso en function cambiaTamano() pongo clase vacía, para que no se repita el efecto de explosión.
+    // al finalizar function explota() continúa enlazando la clase efecto, por eso en function cambiaTamano() elimino la clase efecto, para que no se repita el efecto de explosión.
 }
