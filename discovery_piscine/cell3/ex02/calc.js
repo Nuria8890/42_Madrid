c8513e743a004f1239ab6e98526686eb15aaea86
Nuria Campo <nuria.campo7@gmail.com>
2024-04-09 15:56:06 +0200

    calculadora con comentarios

--- discovery_piscine/cell3/ex02/calc.js
+++ discovery_piscine/cell3/ex02/calc.js
@@ -1,22 +1,27 @@
 
 function comprueba(a) {
-if (Number.isInteger(a) && a >= 0) {
+if (Number.isInteger(a) && a >= 0) { // Number.isInteger es un método que determina si el valor que le paso es entero.
     return true;
 }
 return false;
 }
 
-function calcula() {
+function calcula() { // En html creo un evento "onclick" con esta función.
     console.log ("has pinchado en try me!");
 
-    var firstNumberObj = document.getElementById("firstNumber");
+    var firstNumberObj = document.getElementById("firstNumber"); // Busco en html el elemento con el id indicado entre paréntesis.
     var seconNumberObj = document.getElementById("seconNumber");
-    var firstNumber = Number(firstNumberObj.value);
+    
+    var firstNumber = Number(firstNumberObj.value);// Coge el valor que hay dentro del input. Number() es un constructor que convierte a un string u otro valor en uno de tipo numérico; si el argumento no puede ser convertido a un número, devuelve NaN
     var seconNumber = Number(seconNumberObj.value);
 
-    if (comprueba(firstNumber) && comprueba(seconNumber)) {
+    /* Sería lo mismo que si hago esto:
+    var firstNumber = Number(document.getElementById("firstNumber").value);
+    var seconNumber = Number(document.getElementById("seconNumber").value);*/
 
-        var operador = document.getElementById("operador").value;
+    if (comprueba(firstNumber) && comprueba(seconNumber)) { // Si los dos valores numéricos son enteros y mayores que 0, entra aquí.
+
+        var operador = document.getElementById("operador").value; // Busca en html el elemento con el id operador.
 
         if (operador == "+") {
             sumar(firstNumber, seconNumber);
@@ -38,16 +43,16 @@ function calcula() {
             porcentaje(firstNumber, seconNumber);
         }
     } else {
-        alert("Error :(");
+        alert("Error :(");  // Si los dos valores numéricos NO son enteros y mayores que 0, salta el mensaje de alerta.
     }
 }
 
 
 
-function sumar(a, b) {
+function sumar(a, b) { // Entra en esta función cuando el operador selecionado es +.
     var sumar = a + b;
     console.log ("Estoy sumando y el resultado es " + sumar);
-    alert(sumar);
+    alert(sumar); // El resultado se ve en un mensaje de alerta.
 }
 
 
@@ -67,10 +72,10 @@ function dividir(a, b) {
     var dividir = a / b;
     console.log ("Estoy dividiendo y el resultado es " + dividir);
 
-    if (b === 0) {
+    if (b === 0) { // Si al entrar en esta función, el segundo número es 0, salta un mensaje de alerta con este texto.
         alert("It's over 9000!");
     } else {
-        alert(dividir);
+        alert(dividir); // Si el segundo número es cualquier otro excepto 0, en el mensaje de alerta se verá el resultado de la operación.
     }
 }
 
@@ -85,7 +90,7 @@ function porcentaje(a, b) {
     }
 }
 
-setInterval(ventanaEmergente,30000);
+setInterval(ventanaEmergente,30000); // Cada 30' se abre un mensaje de alerta con el mensaje indicado.
 function ventanaEmergente() {
-    window.alert("Please, use me...")
+    alert("Please, use me...");
 }
